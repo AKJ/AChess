080aa4797b727a9566149dcb0c30b1359da041cb
AKJ <andrew@johnson5.net>
2022-05-16 13:33:49 -0400

    Further game refinements to standardize colors and how moves are handled.

--- achess/game.py
+++ achess/game.py
@@ -20,19 +20,15 @@ class ChessGame():
 		squareData["name"] = chess.square_name(loc)
 		squareData["piece"] = chess.piece_name(board.piece_type_at(loc))
 		if squareData["piece"]:
-			squareData["color"] = self.colors[board.color_at(loc)]
+			squareData["color"] = board.color_at(loc)
 		return squareData
-
-	def getTurn(self, board):
-		return self.colors[board.turn]
 
 	def setFocus(self, board, loc):
 		self.checkLocation(loc)
 		board.focus = loc
 		return loc
 
-	def move(self, board, start, end, promotion=None):
-		capturestring = ""
+	def validateMove(self, board, start, end, promotion=None):
 		self.checkLocation(start)
 		self.checkLocation(end)
 		turn = board.getTurn()
@@ -40,10 +36,6 @@ class ChessGame():
 		endSquare = self.getSquareData(board, end)
 		move = chess.Move(start, end, promotion)
 		if move not in board.legal_moves:
-			output.say("Illegal move!")
+			raise ValueError("Illegal move.")
 		else:
-			board.push(move)
-		movestring = "Moved {} {} from {} to {}".format(startSquare["color"], startSquare["piece"], startSquare["name"], endSquare["name"])
-		if endSquare["piece"]:
-			capturestring = "Captured {}".format(endSquare["piece"])
-		output.say("{}. {}".format(movestring, capturestring))
+			return move, startSquare, endSquare
