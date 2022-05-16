e2262a56e5bbc9d10259dc04fe76093ec3b3e5ca
AKJ <andrew@johnson5.net>
2022-05-16 13:34:34 -0400

    Rewrote the GUI board to utilize new game functions.

--- achess/board.py
+++ achess/board.py
@@ -1,95 +1,65 @@
 import chess
+import game
 import output
 import wx
 
-from keyboard_handler.wx_handler import WXControlKeyboardHandler
-
 class ChessBoard(wx.Panel):
-
-	def __init__(self, parent, id, game, loc, *args, **kwargs):
+	def __init__(self, parent, id, *args, **kwargs):
 		super(ChessBoard, self).__init__(parent, id, *args, **kwargs)
-		self.board = game
-		self.colors = ['Black', 'White']
-		self.handler = WXControlKeyboardHandler(parent)
-		self.location = loc
-		self.marked = ""
-		self.register_keys()
+		self.game = game.ChessGame()
+		self.board = game.createBoard()
+		self.colors = ["Black", "White"]
+		self.marked = None
 
-
-
-	def checkLocation(self):
-		if self.location not in chess.SQUARES:
-			raise ValueError("Error: {} not in chessboard.".format(self.location))
-
+	def announceMove(self, start, end):
+		capturestring = ""
+		movestring = "Moved {} {} from {} to {}".format(self.colors[start["color"]], start["piece"], start["name"], end["name"])
+		if end["piece"]:
+			capturestring = "Captured {}".format(end["piece"])
+		output.say("{}. {}".format(movestring, capturestring))
 
 	def announceSquare(self, loc):
-		self.checkLocation()	
-
-		square = chess.square_name(loc)
-		piece = self.board.piece_at(loc)
-		if piece:
-			pieceColor = self.colors[piece.color]
-			pieceName = chess.piece_name(piece.piece_type)
-			answer = f"{square}, {pieceColor} {pieceName}"
+		square = self.game.getSquareData(self.board, loc)
+		if not square.piece:
+			msg = f"{square.name}"
 		else:
-			answer = f"{square}"
-		output.say(answer)
+			msg = f"{square.name} {square.color} {square.piece}"
+		output.say(msg)
 
-	# Handlers for keyboard keys
-	def register_keys(self):
-		self.handler.register_key("up", self.handle_up)
-		self.handler.register_key("down", self.handle_down)
-		self.handler.register_key("left", self.handle_left)
-		self.handler.register_key("right", self.handle_right)
-		self.handler.register_key("space", self.handle_space)
-		
-	def handle_up(self):
-		self.checkLocation()
-		if self.location not in range(56, 63):
-			self.location = self.location+8
-		self.announceSquare(self.location)
-
-	def handle_down(self):
-		self.checkLocation()
-		if self.location not in range(0, 7):
-			self.location = self.location - 8
-		self.announceSquare(self.location)
-
-	def handle_left(self):
-		self.checkLocation()
-		if self.location not in range(0, 56, 8):
-			self.location = self.location - 1
-		self.announceSquare(self.location)
-
-	def handle_right(self):
-		self.checkLocation()
-		if self.location not in range(7, 63, 8):
-			self.location = self.location + 1
-		self.announceSquare(self.location)
-
-	def handle_space(self):
-		piece = self.board.piece_at(self.location)
-		def mark():
-			self.marked = self.location
-			output.say(f"{self.colors[piece.color]} {chess.piece_name(piece.piece_type)} selected.")
-		if self.marked not in chess.SQUARES:
-			if not self.board.color_at(self.location) == self.board.turn:
-				output.say("Invalid piece.")
+	def mark(self, board, loc):
+		square = game.getSquareData(self.board, loc)
+		if not self.marked:
+			if not square["piece"] or self.board.turn != square["color"]:
+				return
 			else:
-				mark()
-		elif self.marked == self.location:
-			self.marked = ''
-			output.say(f"{chess.piece_name(piece.piece_type)} unselected.")
+				self.marked = loc
+				output.say("{} marked.".format(square["piece"]))
+		elif self.marked and self.marked == loc:
+			self.marked = None
+			output.say("{} unmarked.".format(square["piece"]))
+		elif self.marked and self.marked != loc:
+			move, start, end = game.validateMove(board, loc, self.marked)
+			if move:
+				self.announceMove(self, start, end)
 		else:
-			self.move(self.marked, self.location)
+			output.say("Unexpected error.")
 
-	def move(self, start, end):
-		enemy = self.board.piece_at(end)
-		move = chess.Move(start, end)
-		if move not in self.board.legal_moves:
-			output.say("Illegal move!")
-		else:
-			self.board.push(move)
-			self.marked = ''
-			answer = f"Moved {chess.piece_name(self.board.piece_at(end).piece_type)} from {chess.square_name(start)} to {chess.square_name(end)}"
-			output.say(answer)
+	def navigateBoard(self, board, direction, by="rank"):
+		focus = board.focus
+		if direction == "plus":
+			if by == "rank":
+				result = focus + 1
+			elif by  == "file":
+				result = focus + 8
+		elif direction == "minus":
+			if by == "rank":
+				result = focus - 1
+			elif by == "file":
+				result = focus - 8
+		if result not in chess.SQUARES:
+			self.announceSquare(focus)
+			return
+		focus = result
+		self.announceSquare(result)
+
+	
